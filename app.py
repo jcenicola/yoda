dd4231ceba486737f8d302a9a6603e6243700ceb
John Cenicola <john.cenicola@rackspace.com>
2021-12-09 16:54:59 -0600

    updated to yoda meme

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ AWS_REGION = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/pla
 def fetch_meme_string():
     headers = {
         'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'cookie': '__utmc=71379083; iflipsess=palvmg7qnnf2uoie0vt8hed0kj; __utma=71379083.734663800.1635440230.1637594563.1638385800.10; __utmz=71379083.1638385800.10.9.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __utmt=1; __utmb=71379083.2.9.1638385801000',
+        'cookie': 'iflipsess=2vnh2n6rd3up4oqog91htsv773; __utma=71379083.1107561543.1638889996.1638889996.1638889996.1; __utmb=71379083.36.9.1638892293795; __utmc=71379083; __utmz=71379083.1638889996.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)',
     }
 
-    post_request = 'use_openai=0&meme_id=563423&init_text=&__tok=vBsAiefvLcuv%2BllL2d%2F%2Fwg2r4q4Ou2neXf%2FSrXzVxs4%3D&__cookie_enabled=1'
+    post_request = 'use_openai=0&meme_id=14371066&init_text=&__tok=IQbjUhEGUrbSq2e5rkwPVn8VdeQUTOSb5fzGuY6O30E=&__cookie_enabled=1'
     response = requests.post(
         "https://imgflip.com/ajax_ai_meme",
         data=post_request,
